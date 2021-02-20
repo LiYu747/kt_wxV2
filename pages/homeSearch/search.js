061309b21afe53c4943873b0f2c4fea7bc6f151c
1092216139 <1092216139@qq.com>
2021-02-20 17:59:41 +0800

    2.20

--- pages/homeSearch/search.js
+++ pages/homeSearch/search.js
@@ -1,18 +1,80 @@
 // pages/homeSearch/search.js
+import village from '../../vendor/village/village.js'
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-
+    value: '', //搜索过来的关键词
+    page:1,
+    hasMore: true,
+    locdata:[],
+    isLoding: false
   },
+  // 搜索数据
+  getData() {
+    this.setData({
+      isLoding : true
+    })
+    village.allvillage({
+      data: {
+        page: this.data.page,
+        kw: this.data.value
+      },
+      fail: () => {
+        this.setData({
+          isLoding : false
+        })
+        wx.showToast({
+          title: '网络错误',
+          icon: 'none'
+        })
+      },
+      success: (res) => {
+        this.setData({
+          isLoding : false
+        })
+        if (res.statusCode != 200) return;
 
+        if (res.data.code != 200) return;
+
+        let data = res.data.data
+        this.setData({
+          page :data.current_page + 1,
+          hasMore:data.next_page_url ? true : false,
+          locdata:this.data.locdata.concat(data.data)
+        })
+      }
+    })
+  },
+  // 搜索
+  confirm() {
+    this.setData({
+      page:1,
+      locdata:[]
+    })
+    this.getData()
+  },
+  inputchange(val){
+     this.setData({
+       value:val.detail.value
+     })
+  },
+  // 返回
+  remove(){
+     wx.navigateBack({
+       delta:1
+     })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-     console.log(options)
+     this.setData({
+       value : options.text
+     })
+     this.getData()
   },
 
   /**
@@ -54,7 +116,7 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-
+    this.getData()
   },
 
   /**
